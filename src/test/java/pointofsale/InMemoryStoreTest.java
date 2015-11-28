d972d7042221601dcd19126a1ea9ac50f74a85ec
eparlato <eparlato82@gmail.com>
2015-11-28 11:10:47 +0100

    InMemoryStore item not found test added

--- src/test/java/pointofsale/InMemoryStoreTest.java
+++ src/test/java/pointofsale/InMemoryStoreTest.java
@@ -1,6 +1,6 @@
 package pointofsale;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 public class InMemoryStoreTest {
 
+	
 	@Test
 	public void itemFound() throws Exception {
 		Map<String, Item> items = new HashMap<String, Item>();
@@ -18,4 +19,15 @@ public class InMemoryStoreTest {
 		
 		assertEquals(store.getItem("qwerty1").getPrice(), "$5.65");
 	}
+	
+	@Test
+	public void itemNotFound() throws Exception {
+		Map<String, Item> items = new HashMap<String, Item>();
+
+		Store store = new InMemoryStore(items);
+
+		assertNull(store.getItem("zxcv987"));
+	}
+	
+	
 }
