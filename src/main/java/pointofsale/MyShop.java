3c20010602b1903ee451f59a15e69cf700de3a6f
eparlato <eparlato82@gmail.com>
2015-11-29 21:51:03 +0100

    Removed unnecessary static objects and methods from MyShop

--- src/main/java/pointofsale/MyShop.java
+++ src/main/java/pointofsale/MyShop.java
@@ -4,14 +4,17 @@ import java.util.HashMap;
 
 public class MyShop {
 
-	static Store store;
-	static Display display;
-	static PointOfSale pos;
-	
-	public static void main(String[] args) {
+	Store store;
+	Display display;
+	PointOfSale pos;
 
-		setup();
-		
+	public static void main(String[] args) {
+		MyShop myShop = new MyShop();
+		myShop.setup();
+		myShop.scanAFewBarcodes();
+	}
+
+	private void scanAFewBarcodes() {
 		pos.onBarcode("qwerty1");
 		pos.onBarcode("zxcvbn3");
 		pos.onBarcode("");
@@ -19,13 +22,13 @@ public class MyShop {
 		pos.onBarcode("lkjhgf");
 	}
 
-	private static void setup() {
+	private void setup() {
 
 		HashMap<String, Item> items = new HashMap<String, Item>();
 		items.put("qwerty1", new Item("qwerty1", "$10.0"));
 		items.put("asdfgh2", new Item("asdfgh2", "$20.0"));
 		items.put("zxcvbn3", new Item("zxcvbn3", "$30.0"));
-		
+
 		store = new InMemoryStore(items);
 		display = new ConsoleDisplay();
 		pos = new PointOfSale(display, store);
